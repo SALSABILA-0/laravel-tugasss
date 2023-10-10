debc542d555ec3a0841df0c05f161d8ad51f0011
SALSABILA-0 <115692369+SALSABILA-0@users.noreply.github.com>
2023-10-10 09:05:00 +0700

    Delete resources directory

--- resources/views/layout/isipengaduan.controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PengaduanController;
-use App\Http\Controllers\MasyarakatController;
-
-Route::get('/home', [PengaduanController::class, 'index']);
-Route::get('/isipengaduan', [PengaduanController::class, 'tampil_isi_pengaduan']);
-Route::post('/isipengaduan', [PengaduanController::class, 'proses_isi_pengaduan']);
